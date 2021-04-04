de5e546a6ccf3fc8e6f4ffa512451e6de86884c3
ara <ehdkfk@nate.com>
2021-04-04 13:30:50 +0900

    0404 도아라

--- Baekjoon/Q13163_ChangeToGod.java
+++ Baekjoon/Q13163_ChangeToGod.java
@@ -1,7 +1,7 @@
 package Baekjoon;
 
 import java.util.*;
-public class Q13163_changeToGod {
+public class Q13163_ChangeToGod {
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
